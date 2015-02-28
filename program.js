ae91d9cfd387ceb58d26d387bd0c8c5b56bcd371
CADBOT <charliec364@gmail.com>
2015-02-27 20:50:59 -0500

    Create command processor

--- program.js
+++ program.js
@@ -3,59 +3,69 @@ var https = require('https')
 var querystring = require('querystring');
 var app = express()
 
-function command_shippable(command) {
-  var str = ''
-//curl -H "Authorization: apiToken $apiToken" https://apibeta.shippable.com/accounts/54e2e00591426fd6a78cf0df/projects | jq -r '.[] | select(.name == "kicke").
-  var project = '54e80f5d91426fd6a78f6281'
+var commands = {
+  build: build
+}
+
+function build(project) {
+ console.log("building " + project) 
   var options = {
     hostname: 'apibeta.shippable.com',
-    path: "/projects/" + project + "/recentBuilds",
-    method: 'GET',
-    headers: {'Authorization': 'apiToken 69e97a32-723d-4dd8-b84f-bf75d6a838e0'}
+    path: "/projects/" + project + "/builds/new",
+    method: 'POST',
+    headers: {'Authorization': 'apiToken ' + process.env[apiToken]}
   }
   var callback = function(response) {
-    var str = ''
+    var output = ''
     response.on('data', function(chunk) {
-      str += chunk
+      output += chunk
     })
 
     response.on('end', function () {
-      console.log(str)
-      var options = {
-        hostname: 'hooks.slack.com',
-        path: '/services/T03QGDMRG/B03RENCT1/5XIFxu0dsJzsHfKwOb7Ix4Zv',
-        method: 'GET',
-        headers: { 
-          //'Content-Type': 'application/json',
-          'payload': {'text': 'hello from slack'} 
-        }
-      }
-      https.request(options, function(res) {
-        console.log("statusCode: ", res.statusCode);
-        console.log("headers: ", res.headers);
-
-
-        res.on('data', function(d) {
-          process.stdout.write(d);
-        });
-      }).end()
+      write_result_to_slack(output)
     })
   }
   https.request(options, callback).end()
+}
 
+function command_processor(command_str) {
+  var command_arr = command_str.split(' ')
+  var command = command_arr[0]
+  var parameter = command_arr[1]
+  commands[command](parameter)
+}
+
+function write_result_to_slack(output) {
+  console.log(output)
+  var options = {
+    hostname: 'hooks.slack.com',
+    path: '/services/T03QGDMRG/B03RENCT1/5XIFxu0dsJzsHfKwOb7Ix4Zv',
+    method: 'GET',
+    headers: { 
+      'payload': "{'text': 'hello from slack'}"
+    }
+  }
+  var payload = {text: 'Hello from middleware!'}
+  var body_data = querystring.stringify(payload)
+  var req = https.request(options, function(res) {
+    console.log("statusCode: ", res.statusCode);
+    console.log("headers: ", res.headers);
+
+
+    res.on('data', function(d) {
+      process.stdout.write(d);
+    });
+  })
+  req.write(body_data)
+  req.end()
 }
 
 app.get('/slack', function(req, res) {
   res.send(req.body)
-  //console.log(req.body)
-  console.log('hello from slack')
-  //console.log(req.headers)
-  //console.log(req.body)
-  //console.log(req.query)
   console.log(req.query.text)
   if (req.query.token == 'ML6frujgk0ekVKkNfM77qwlm') {
-    var command =req.query.text
-    command_shippable(command)
+    var command_str =req.query.text
+    command_processor(command_str)
   }
 })
 
